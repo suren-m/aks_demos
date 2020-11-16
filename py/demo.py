2a94f7e4010747c85eb1d3673cfef30f08acf815
suren <suren@sp4.localdomain>
2020-11-16 17:11:09 +0000

    redis demo using python

---
+++ py/demo.py
@@ -0,0 +1,7 @@
+a = [1,2,3,4,5]
+
+
+b = [x for x in a]
+
+print(a)
+
---
+++ py/redis/main.py
@@ -0,0 +1,16 @@
+import requests
+import redis
+import os
+
+r = redis.Redis(
+    host='akscache.redis.cache.windows.net',
+    port='6380',
+    password=os.environ['CACHE_CRED'],
+    ssl=True
+    )
+
+r.set('foo','demos')
+
+print(r.get('foo'))
+
+print("done")
