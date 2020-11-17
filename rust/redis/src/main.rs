dd1fe67510d48dbb14e87443bec8d4b27b5d5ce0
suren <suren>
2020-11-17 15:49:44 +0000

    rust client update

--- rust/redis/src/main.rs
+++ rust/redis/src/main.rs
@@ -1,26 +1,23 @@
 use std::env;
-
 extern crate redis;
 use redis::Commands;
 
 // https://redislabs.com/lp/redis-enterprise-rust/
 // https://github.com/mitsuhiko/redis-rs
 
-fn main() { 
-    let addr = "akscache.redis.cache.windows.net";
-    let cred = env::var("CACHE_CRED").unwrap();
-    let port = 6380;
-    let url = format!(
-        "redis://u:{}@{}:{}",
-        cred,
-        addr,
-        port);
+fn main() {
+    let hostname = env::var("CACHE_HOST").unwrap();
+    let passwd = env::var("CACHE_KEY").unwrap();
     
+    let port = 6379;
+    let url = format!(
+        "redis://{}@{}.redis.cache.windows.net:{}",
+        passwd, hostname, port
+    );
+
     println!("{}", url);
-    /*
     let client = redis::Client::open(url).unwrap();
     let mut con = client.get_connection().unwrap();
     let res: String = con.get("foo").unwrap();
     println!("Res: {}", res);
-    */
 }
